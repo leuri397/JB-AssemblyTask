eb32dc000c5590401e1cc6d2d1bc3929272d88fc
leuri <leouri2001@gmail.com>
2021-03-06 15:42:06 +0300

    brush up an example

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
 extern "C" int lineFinder ( char* array, int width, int height);
 int main() {
-	char field[][6] = {
-		{0, 0, 0, 0, 0, 0},
-		{3, 1, 3, 0, 1, 1},
-		{0, 1, 0, 0, 3, 2},
-		{3, 3, 2, 1, 1, 4},
-		{4, 2, 4, 1, 0, 2},
-		{1, 3, 2, 3, 3, 0}
-	};
-	int width = sizeof(field[0]), height = sizeof(field)/sizeof(field[0]);
-	std::cout << lineFinder((char*)field, width, height) << std::endl;
+	int width, height;
+	std::cout << "Enter array dimensions: ";
+	std::cin >> width >> height;
+	char* field = new char[width*height];
+	for (int i = 0; i < height; i++)
+	{
+		std::cout << "Enter array line: ";
+		for (int j = 0; j < width; j++)
+		{
+			char buffer;
+			std::cin >> buffer;
+			field[i*width + j] = buffer - '0';
+		}
+		std::cout << std::endl;
+	}
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			std::cout << (int)field[i*width + j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	int result = lineFinder((char *)field, width, height);
+	if (result <= 1)
+	{
+		std::cout << "There is no vertical lines in array!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Maximal line length in aray: " << result << std::endl;
+	}
+	delete[] field;
 }
