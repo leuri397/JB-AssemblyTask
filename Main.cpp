58e0ba85cf4d7c1725ae0562ab4073c099eec704
leuri <leouri2001@gmail.com>
2021-03-06 13:22:33 +0300

    barely not working version

--- Main.cpp
+++ Main.cpp
@@ -6,8 +6,8 @@ int main() {
 		{8, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 6},
 		{4, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 2},
-		{1, 0, 0, 0, 0, 0}
+		{0, 1, 1, 1, 1, 2},
+		{1, 1, 1, 1, 1, 1}
 	};
 	int width = sizeof(field[0]), height = sizeof(field)/sizeof(field[0]);
 	std::cout << lineFinder((char*)field, width, height - 2) << std::endl;
--- function.s
+++ function.s
@@ -13,33 +13,43 @@ lineFinder:
 	xor %rcx, %rcx
 	xor %rdx, %rdx
 	xor %r9, %r9
+	xor %r10, %r10
 // rcx - horisontal iteration
 // rdx - vertical iteration
-width_loop:
-	xor %rcx, %rcx
-height_loop:
-	call get_element
-	add %rax, %r9
 
-	inc %rcx
-	cmp 8(%rsp), %rcx
-	jl height_loop
+	width_loop:
+		xor %rcx, %rcx
+		height_loop:
+			mov 8(%rsp), %rax
+			mov 16(%rsp), %rbx
+			call get_element
+			cmp $0, %rax
+			jne continue
+			mov 8(%rsp), %rax
+			mov (%rsp), %rbx
+			mov 16(%rsp), %r8
+			call follow_line
+			cmp %r10, %rax
+			jl continue
+			mov %rax, %r10
+		continue:
+			inc %rcx
+			cmp 8(%rsp), %rcx
+			jl height_loop
 	
-	inc %rdx
-	cmp  (%rsp), %rdx
-	jl width_loop
+		inc %rdx
+		cmp  (%rsp), %rdx
+		jl width_loop
 	
-	mov %r9, %rax
+	mov %r10, %rax
 	
 	add $24, %rsp
 	ret
 // rcx, rdx - iterators for position
+// rax - width
+// rbx - address
 // rax - result
 get_element:
-	push %rbx
-	xor %rax, %rax
-	mov 32(%rsp), %rbx
-	mov 24(%rsp), %rax
 	push %rdx
 	mul %rdx
 	pop %rdx
@@ -47,8 +57,47 @@ get_element:
 	add %rax, %rbx
 	xor %rax, %rax
 	movb (%rbx), %al
-	pop %rbx
 	ret
+	
+	
+// rcx, rdx - start position
+// rax - width
+// rbx - height
+// r8 - address
+// rax - result	
+follow_line:
+	push %rdx
+	push %rax
+	push %rbx
+	push %r8
+	mov $1, %r9
+	/*
+	stack:
+		0 - address
+		8 - height
+		16 - width
+		24 - old rdx
+	*/
+	cmp 8(%rsp), %rdx
+	jge finish
+	
+	mov (%rsp), %rbx
+downloop:
+	mov 16(%rsp), %rax
+	call get_element
+	cmp $0, %rax
+	jne finish
+	inc %r9
+
+	inc %rdx
+	cmp 8(%rsp), %rdx
+	jl downloop
+	
+	finish:
+	add $24, %rsp
+	pop %rdx
+	mov %r9, %rax
+		ret
 	
 
 	
