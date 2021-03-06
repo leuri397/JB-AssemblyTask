41bbdb24715ec772fc478e8165910964569a86c6
leuri <leouri2001@gmail.com>
2021-03-06 15:08:37 +0300

    example changed

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,13 @@
 #include <iostream>
 extern "C" int lineFinder ( char* array, int width, int height);
 int main() {
-	char field[][5] = {
-		{4, 1, 5, 1, 1},
-		{1, 2, 1, 1, 1},
-		{1, 1, 1, 0, 1},
-		{1, 1, 1, 0, 1},
-		{1, 1, 1, 0, 1},
-		{1, 0, 1, 1, 1},
-		{1, 0, 3, 1, 1},
-		{1, 0, 1, 5, 1},
-		{1, 0, 1, 5, 1},
-		{1, 0, 2, 1, 1},
-		{1, 1, 1, 1, 1},
-		{1, 1, 1, 1, 1},
-		{1, 0, 1, 1, 1},
-		{1, 0, 1, 1, 1},
-		{1, 0, 1, 1, 1}
+	char field[][6] = {
+		{0, 0, 0, 0, 0, 0},
+		{3, 1, 3, 0, 1, 1},
+		{0, 1, 0, 0, 3, 2},
+		{3, 3, 2, 1, 1, 4},
+		{4, 2, 4, 1, 0, 2},
+		{1, 3, 2, 3, 3, 0}
 	};
 	int width = sizeof(field[0]), height = sizeof(field)/sizeof(field[0]);
 	std::cout << lineFinder((char*)field, width, height) << std::endl;
